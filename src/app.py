04ba46e67bfd4f228840cbe2df00e5ae6f5e4fda
harish-167 <harishkamble167@gmail.com>
2025-06-08 15:46:04 +0530

    Flask is accessible in local network i.e. 0.0.0.0

--- src/app.py
+++ src/app.py
@@ -33,4 +33,4 @@ def generate_text():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True) # debug=True allows for automatic reloads on code changes
+    app.run(debug=True,host='0.0.0.0',port=5000) # debug=True allows for automatic reloads on code changes
